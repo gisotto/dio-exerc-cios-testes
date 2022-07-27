30a87ecc7b80e1166994d6f7328d734e81e8f3a5
gisotto <gi@sotto.com.br>
2022-07-26 23:26:09 -0300

    .

---
+++ operadores/teste.js
@@ -0,0 +1,39 @@
+function comparaNumeros(num1, num2) {
+    const primeiraFrase = criePrimeiraFrase(num1,num2);
+    const segundaFrase = crieSegundaFrase(num1,num2);
+
+    return `${primeiraFrase} ${segundaFrase}`
+}
+
+function criePrimeiraFrase(num1, num2) {
+    let saoIguais = '';
+
+    if(num1 !=== num2) {
+        saoIguais = 'não';
+    }
+
+    return `Os números ${num1} e ${num2} ${saoIguais} são iguais. `
+};
+
+function crieSegundaFrase(num1, num2){
+    const soma = num1 + num2;
+
+    let resultado10 = 'menor';
+    let resultado20 = 'menor';
+
+    const comparacao10 = soma > 10;
+    const comparacao20 = soma > 20;
+
+    if(comparacao10) {
+        resultado10 = 'maior';
+    }
+
+    if(comparacao20) {
+        resultado20 = 'maior';
+    }
+     this
+
+    return `Sua soma é ${soma}, que é{resultado} que 10 e ${resultado20} que 20.`
+}
+
+console.log(comparaNumeros(20, 20));
